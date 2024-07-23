9cb06c6bf8e889397d7af080c09fcc758e34fc3e
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-07-24 01:39:25 +0530

    Bugs Solved

--- script.js
+++ script.js
@@ -130,16 +130,26 @@ searchForm.addEventListener("submit", (e) => {
 async function fetchWeatherByCity(city){
     try{
         loadingScreen.classList.add("active");
-        userInfoContainer.classList.add("active");
+        
+        userInfoContainer.classList.remove("active");
         grantacessbtn.classList.remove("active");
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`);
         const data = await response.json();
         loadingScreen.classList.remove("active");
-        renderWeather(data);
+        if(data.cod === "404"){
+            alert("City not found");
+            userInfoContainer.classList.remove("active");
+            return;
+        }
+        else{
+            userInfoContainer.classList.add("active");
+           
+            renderWeather(data);
+        }
     }
     catch(error){
-        console.log("Error: ", error);
+        userInfoContainer.classList.remove("active");
     }
 }
 
